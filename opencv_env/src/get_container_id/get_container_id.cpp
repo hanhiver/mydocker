3f4a11779f1a16a5e2472f08c1b47815e1f954e8
hanhiver <handongfr@163.com>
2019-12-19 15:31:17 +0800

    update

---
+++ opencv_env/src/get_container_id/get_container_id.cpp
@@ -0,0 +1,67 @@
+#include <iostream> 
+#include <fstream> 
+#include <regex> 
+
+std::string get_container_id()
+{
+    std::string container_id {""};  
+
+    std::ifstream cgroup_file; 
+    std::string cgroup_file_name = "/proc/1/cgroup";
+
+    cgroup_file.open(cgroup_file_name); 
+    if (!cgroup_file.is_open())
+    {
+        std::cout << "Error opening file: " 
+                  << cgroup_file_name << std::endl; 
+        return container_id;
+    }
+
+    auto cgroup_line1_reg = std::regex("1:name=systemd:/"); 
+    std::smatch match;  
+
+    std::string line;
+    while (std::getline(cgroup_file, line))
+    {
+        if (std::regex_search(line, match, cgroup_line1_reg))
+        {
+            std::string suffix(match.suffix().str()); 
+            //std::cout << suffix << std::endl; 
+            
+            auto docker_pos = suffix.find("docker");
+            if ( suffix.length() > 70 && \
+                docker_pos <= suffix.length())
+            {
+                container_id = suffix.substr(docker_pos+7, docker_pos+70); 
+                /*
+                std::cout << "In a container. " << std::endl; 
+                std::cout << docker_pos << std::endl; 
+                std::cout << container_id << std::endl;
+                */
+            }
+        }
+    }
+
+    cgroup_file.clear();
+    cgroup_file.close();
+
+    return container_id; 
+}
+
+
+int main(int argc, char* argv[])
+{
+    std::string docker_id = get_container_id(); 
+
+    if (!docker_id.empty())
+    {
+        std::cout << "In a container, ID: " 
+                  << get_container_id() << std::endl;
+    }
+    else
+    {
+        std::cout << "Not in a container. " << std::endl; 
+    }
+     
+    
+}
