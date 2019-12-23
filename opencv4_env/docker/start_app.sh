ef2ee67a7ae10502e4067fc2dcd6f3a1b9e5fa5b
hanhiver <handongfr@163.com>
2019-12-23 16:44:14 +0800

    update

---
+++ opencv4_env/docker/start_app.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export LD_LIBRARY_PATH=/usr/local/nvidia/lib64/
+cd /opt/
+
+while true
+do
+    echo "----------Start App ------" 
+    echo "----------App Exit ------"
+    sleep 100
+done
+
+exit 1 
