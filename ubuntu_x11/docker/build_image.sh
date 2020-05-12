2cff95f7032cf263f0b4c43b98b37077bd6e003f
hanhiver <handongfr@163.com>
2020-05-12 17:14:47 +0800

    update

---
+++ ubuntu_x11/docker/build_image.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -x
+
+DIR=$(cd `dirname $0`; pwd)
+
+CPU_ARCH=$(uname -m)
+
+if [ $CPU_ARCH == "x86_64" ]; then
+    image_release_name=ubuntu_x11
+    dockerfile=Dockerfile
+else
+    echo "Unsupported CPU architecture: $CPU_ARCH"
+    exit 1
+fi
+
+if [ $VERSION ]; then
+    image_release_ver=$VERSION
+else
+    image_release_ver=dev
+fi
+
+# Build release package image
+docker build --network host \
+    --build-arg https_proxy=$https_proxy --build-arg http_proxy=$http_proxy \
+    --build-arg HTTP_PROXY=$HTTP_PROXY --build-arg HTTPS_PROXY=$HTTP_PROXY \
+    --build-arg NO_PROXY=$NO_PROXY  --build-arg no_proxy=$no_proxy \
+    -t ${image_release_name}:${image_release_ver} -f ${dockerfile} .
+
+# --build-arg http_proxy=http://9.111.143.18:3228 \
+# --build-arg https_proxy=http://9.111.143.18:3228 \
+# -t ${image_release_name}:${image_release_ver} -f ${dockerfile} .
